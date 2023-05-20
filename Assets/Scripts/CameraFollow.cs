aee1723d58667881927d86297f25027ade2f751f
Adheesh C <adheesh@vangoghimaging.com>
2023-05-19 20:16:40 -0400

    init commit

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{ 
+    public GameObject player;
+    public float cameraDistance = 10;
+    // Update is called once per frame
+    void LateUpdate() {
+        gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, new Vector3(0, gameObject.transform.position.y, player.gameObject.transform.position.z - cameraDistance), Time.deltaTime * 100);
+    }
+
+}
